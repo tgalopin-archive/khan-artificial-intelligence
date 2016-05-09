77043e05f7b605f708ea9ac56035608566526006
Titouan Galopin <galopintitouan@gmail.com>
2016-05-09 08:28:15 +0200

    Merge changes

--- main.pl
+++ main.pl
@@ -7,7 +7,13 @@ initBoard(2, [ [2,3,1,2,2,3], [2,1,3,1,3,1], [1,3,2,3,1,2], [3,1,2,1,3,2], [2,3,
 initBoard(3, [ [2,3,1,2,2,3], [2,1,3,1,3,1], [1,3,2,3,1,2], [3,1,2,1,3,2], [2,3,1,3,1,3], [2,1,3,2,2,1] ]). % TODO
 initBoard(4, [ [2,3,1,2,2,3], [2,1,3,1,3,1], [1,3,2,3,1,2], [3,1,2,1,3,2], [2,3,1,3,1,3], [2,1,3,2,2,1] ]). % TODO
 
-% Rotate the board
+% Possibles pieces
+piece(sr1).
+piece(sr2).
+piece(sr3).
+piece(sr4).
+piece(sr5).
+piece(kr).
 
 % Display the board
 displayRow([]).
@@ -17,6 +23,21 @@ displayRows([L|[]]) :- write(' -------------------------'), nl, displayRow(L), n
 displayRows([L|Q]) :- write(' -------------------------'), nl, displayRow(L), nl, displayRows(Q).
 
 displayBoard(P) :- displayRows(P).
+
+% Replace the element i from the the list T by X and return result in [X|T]
+replace([_|T], 0, X, [X|T]).
+replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
+replace(L, _, _, L).
+
+% Replace the piece is place occupied
+addPiece(L2,Pie, L3) :- length(L2,2),nth0(0,L2,L2A), append([L2A],Pie,L3),!.
+
+% Add the piece is place free
+addPiece(L2, Pie, L3) :- append([L2],Pie,L3),!.
+
+% Move a piece
+movePiece(Pie,X,Y,NP, NP2) :- board(Z), Z = NP, nth0(X, Z, L1), nth0(Y, L1, L2), addPiece(L2, Pie, L3), replace(L1, Y, L3, L4), replace(Z, X, L4, NP2).
+movePiece(Pie,X,Y,NP, NP2) :- nth0(X, NP, L1), nth0(Y, L1, L2), addPiece(L2, Pie, L3), replace(L1, Y, L3, L4), replace(NP, X, L4, NP2).
 
 % Main process
 main(_) :-
@@ -32,23 +53,7 @@ main(_) :-
 
 
 
-
-/*
-printHelloWorld :-
-  write('Hello world !'),
-  nl.
-  */
-  
-% initialisation du tableau  
- 
- 
- 
-
-
-% Avec appel initBoard(Y) on pourra utiliser Y comme la liste du board
-
-/*text(1) :-.
-text(2) :-.
-text(3) :-.
-
-choix_piece():- read(X), .*/
+% Idées:
+% place([T|[]]) :- write(T), write(' = (X,Y)'), nl, read(X), read(Y), move(T,X,Y,).
+% place([T|Q]) :- write(T), write(' = (X,Y)'), nl, read(X), read(Y), insertBoard(T,X,Y), place(Q).
+% initBoard(_) :- board(X), place(X,Y), show_board(Y).
